c754164d0ba3cb600b9abac6f48aa0defaa3fbcb
Claire Lovisa <lovisa.claire@gmail.com>
2020-05-19 11:57:08 +0200

    Make the service complient with download service

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 import { app, errorHandler, uuid, query, update, sparqlEscapeString, sparqlEscapeDateTime } from 'mu';
-import { findAllFileAddressesToHarvest, harvestFileAddress, finishHarvestCollections } from './lib/harvest';
+import { findAllFilesToHarvest, harvestFile, finishHarvestCollections } from './lib/harvest';
 
 /**
- * Harvests downloaded file addresses that have not been harvested before.
+ * Harvests downloaded files that have not been harvested before.
  * The harvesting may generate new file addresses to be download and harvested.
- * All related file addresses are collected in a harvest collection.
+ * All related files are collected in a harvest collection.
 */
 app.post('/harvest', async function(req, res, next) {
   try {
-    console.log(`Start harvesting new file addresses`);
-    const fileAddresses = await findAllFileAddressesToHarvest();
-    
-    for (let fileAddress of fileAddresses) {
-      await harvestFileAddress(fileAddress);
+    console.log(`Start harvesting new files`);
+    const files = await findAllFilesToHarvest();
+
+    for (let file of files) {
+      await harvestFile(file);
     }
 
-    console.log(`Harvesting done. Time to wrap up!`);    
-    await finishHarvestCollections();
-
     console.log(`We're done! Let's wait for the next harvesting round...`);        
+    return res.status(202).end();
 
-    return res.status(202).end();    
   } catch (e) {
     return next(new Error(e.message));
   }
 });
-
 
 app.use(errorHandler);
 
