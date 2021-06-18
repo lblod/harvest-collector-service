bde18c5d831ab8ce3711ea98de24ee3d76910e0a
Nordine Bittich <nordine@bittich.be>
2021-06-18 08:22:49 +0200

    handle failed url

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-import { app, errorHandler, uuid, query, update, sparqlEscapeString, sparqlEscapeDateTime } from 'mu';
-import { ensureFilesAreReadyForHarvesting, harvestRemoteDataObject, finishHarvestCollections, isRemoteDataObject } from './lib/harvest';
+import { app, errorHandler, uuid, query, update, sparqlEscapeString, sparqlEscapeDateTime, sparqlEscapeUri } from 'mu';
+import { ensureFilesAreReadyForHarvesting, handleDownloadFailure, harvestRemoteDataObject, isRemoteDataObject } from './lib/harvest';
 import flatten from 'lodash.flatten';
 import bodyParser from 'body-parser';
+const SUCCESS = 'http://lblod.data.gift/file-download-statuses/success';
+const FAILURE = 'http://lblod.data.gift/file-download-statuses/failure';
 
-app.use(bodyParser.json({ type: function(req) { return /^application\/json/.test(req.get('content-type')); } }));
+app.use(bodyParser.json({ type: function (req) { return /^application\/json/.test(req.get('content-type')); } }));
+
+
+app.post("/on-download-failure", async (req, res, next) => {
+  const remoteDatasMaxFailure = await getRemoteFileUris(req.body, FAILURE);
+  if (!remoteDatasMaxFailure.length) {
+  } else {
+    await handleDownloadFailure(remoteDatasMaxFailure);
+  }
+  return res.status(200).send();
+});
 
 /**
  * Harvests downloaded files that have not been harvested before.
  * The harvesting may generate new remote data to be download and harvested.
  * All related files are collected in a harvest collection.
 */
-app.post('/delta', async function(req, res, next) {
-  const remoteFiles = await getRemoteFileUris(req.body);
+app.post('/delta', async function (req, res, next) {
+  const remoteFiles = await getRemoteFileUris(req.body, SUCCESS);
   if (!remoteFiles.length) {
     console.log("Delta does not contain a new remote data object with status 'success'. Nothing should happen.");
     return res.status(204).send();
@@ -41,9 +53,9 @@ app.post('/delta', async function(req, res, next) {
  *
  * @param Object delta Message as received from the delta notifier
 */
-async function getRemoteFileUris(delta) {
+async function getRemoteFileUris(delta, status) {
   const inserts = flatten(delta.map(changeSet => changeSet.inserts));
-  const successes = inserts.filter(isTriggerTriple).map(t => t.subject.value);
+  const successes = inserts.filter(triple => isTriggerTriple(triple, status)).map(t => t.subject.value);
   const remoteDataObjects = (await Promise.all(successes.map(uri => isRemoteDataObject(uri)))).filter(object => object != undefined);
   return remoteDataObjects;
 }
@@ -53,9 +65,11 @@ async function getRemoteFileUris(delta) {
  *
  * @param Object triple Triple as received from the delta notifier
 */
-function isTriggerTriple(triple) {
+function isTriggerTriple(triple, status) {
   return triple.predicate.value == 'http://www.w3.org/ns/adms#status'
-    && triple.object.value == 'http://lblod.data.gift/file-download-statuses/success';
+    && triple.object.value == status;
 };
+
+
 
 app.use(errorHandler);
