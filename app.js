f27ef2f0db3ee4e0f7238172074e3421d3227ada
felix <ruiz.felixruiz@gmail.com>
2021-09-20 15:35:53 +0200

    Update controller logic to deal with how
    tasks are scheduled for collecting information.
    
    Previously, this service listened to the status files ready.
    This has changed because of the scheduledJobs.
    
    The current change is reverse compatible though

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-import { app, errorHandler, uuid, query, update, sparqlEscapeString, sparqlEscapeDateTime, sparqlEscapeUri } from 'mu';
-import { ensureFilesAreReadyForHarvesting, handleDownloadFailure, harvestRemoteDataObject, isRemoteDataObject } from './lib/harvest';
-import flatten from 'lodash.flatten';
+import { updateSudo as update } from '@lblod/mu-auth-sudo';
 import bodyParser from 'body-parser';
+import flatten from 'lodash.flatten';
+import { app, errorHandler, sparqlEscapeUri } from 'mu';
+import {
+    PREFIXES, STATUS_BUSY, STATUS_FAILED, STATUS_READY_TO_BE_CACHED, STATUS_SCHEDULED, TASK_COLLECTING, TASK_TYPE
+} from './constants';
+import { Delta } from './lib/delta';
+import { ensureFilesAreReadyForHarvesting, handleDownloadFailure, harvestRemoteDataObject, isRemoteDataObject } from './lib/harvest';
+import { appendTaskError, isTask, loadTask, updateTaskStatus } from './lib/task';
+
+
+
 const SUCCESS = 'http://lblod.data.gift/file-download-statuses/success';
 const FAILURE = 'http://lblod.data.gift/file-download-statuses/failure';
 
@@ -23,6 +32,14 @@ app.post("/on-download-failure", async (req, res, next) => {
  * All related files are collected in a harvest collection.
 */
 app.post('/delta', async function (req, res, next) {
+  //Delta may contain mutiple blobs of information. Hence this block of code does two things
+
+  //Handle new tasks
+  const entries = new Delta(req.body).getInsertsFor('http://www.w3.org/ns/adms#status', STATUS_SCHEDULED);
+  await startCollectingTasks(entries);
+
+
+  //Handle the follow up tasks, i.e. make sure to stop once all URLs downloaded.
   const remoteFiles = await getRemoteFileUris(req.body, SUCCESS);
   if (!remoteFiles.length) {
     console.log("Delta does not contain a new remote data object with status 'success'. Nothing should happen.");
@@ -69,5 +86,84 @@ function isTriggerTriple(triple, status) {
   return triple.predicate.value == 'http://www.w3.org/ns/adms#status'
     && triple.object.value == status;
 };
+
+
+async function startCollectingTasks(entries){
+  for (let entry of entries) {
+    if(! await isTask(entry) ) continue;
+    const task = await loadTask(entry);
+
+    try {
+      if(isCollectingTask(task)){
+        await updateTaskStatus(task, STATUS_BUSY);
+        await scheduleRemoteDataObjectsForDownload(task);
+      }
+    }
+    catch (e){
+      console.error(e);
+      await appendTaskError(task, e.message);
+      await updateTaskStatus(task, STATUS_FAILED);
+    }
+  }
+}
+
+function isCollectingTask(task){
+   return task.operation == TASK_COLLECTING;
+}
+
+async function scheduleRemoteDataObjectsForDownload(task){
+  const deleteStatusQuery = `
+    ${PREFIXES}
+
+    DELETE {
+      GRAPH ?g {
+        ?remoteDataObject adms:status ?status.
+      }
+    }
+    WHERE {
+     BIND(${sparqlEscapeUri(task.task)} as ?task)
+     GRAPH ?g {
+       ?remoteDataObject adms:status ?status.
+     }
+
+     ?task a ${ sparqlEscapeUri(TASK_TYPE) };
+       task:inputContainer ?container.
+
+     ?container task:hasHarvestingCollection ?collection.
+     ?collection a hrvst:HarvestingCollection.
+     ?collection dct:hasPart ?remoteDataObject.
+     ?remoteDataObject a nfo:RemoteDataObject.
+  }
+  `;
+
+  await update(deleteStatusQuery);
+
+  const updateQuery = `
+    ${PREFIXES}
+
+    INSERT {
+      GRAPH ?g {
+        ?remoteDataObject adms:status ${sparqlEscapeUri(STATUS_READY_TO_BE_CACHED)}.
+      }
+    }
+    WHERE {
+     BIND(${sparqlEscapeUri(task.task)} as ?task)
+
+     ?task a ${ sparqlEscapeUri(TASK_TYPE) };
+       task:inputContainer ?container.
+
+     ?container task:hasHarvestingCollection ?collection.
+     ?collection a hrvst:HarvestingCollection.
+     ?collection dct:hasPart ?remoteDataObject.
+
+     GRAPH ?g {
+       ?remoteDataObject a nfo:RemoteDataObject.
+     }
+  }
+  `;
+
+  await update(updateQuery);
+}
+
 
 app.use(errorHandler);
