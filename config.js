a60641e257302d07d58ceebd23f315c40f6f67cb
Claire Lovisa <lovisa.claire@gmail.com>
2022-11-21 12:40:30 +0100

    Adjust cron job

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const CRON_FREQUENCY = process.env.CRON_FREQUENCY || '19 * * * *';
+export const CRON_FREQUENCY = process.env.CRON_FREQUENCY || '*/5 * * * *';
 export const DELETE_BATCH_SIZE = parseInt(process.env.DELETE_BATCH_SIZE) || 1000;
 export const SCHEDULED_TASK_CREATOR = process.env.SCHEDULED_TASK_CREATOR  || 'http://lblod.data.gift/services/migrations';
