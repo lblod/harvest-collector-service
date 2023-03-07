4c04d1323c41cf6471cab37dfc8bc5e89ceac1ac
David <david.journo.31@gmail.com>
2023-03-07 17:23:34 +0100

    Adding new helper functions
    - hasAuth is essentially checking if the authenticationConfiguration has a securityType
    
    - attachClonedAuthenticationConfiguraton is cloning authentication information into a new authenticationConfiguration so this can be added to the right remoteDataObject

--- constants.js
+++ constants.js
@@ -11,10 +11,20 @@ export const ERROR_TYPE= 'http://open-services.net/ns/core#Error';
 
 export const ERROR_URI_PREFIX = 'http://redpencil.data.gift/id/jobs/error/';
 
+export const BASIC_AUTH = 'https://www.w3.org/2019/wot/security#BasicSecurityScheme';
+export const OAUTH2 = 'https://www.w3.org/2019/wot/security#OAuth2SecurityScheme';
+
 export const PREFIXES = `
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX muAccount: <http://mu.semte.ch/vocabularies/account/>
+  PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
   PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
   PREFIX dct: <http://purl.org/dc/terms/>
+  PREFIX dgftSec: <http://lblod.data.gift/vocabularies/security/>
+  PREFIX dgftOauth: <http://kanselarij.vo.data.gift/vocabularies/oauth-2.0-session/>
+  PREFIX wotSec: <https://www.w3.org/2019/wot/security#>
+  PREFIX rpioHttp: <http://redpencil.data.gift/vocabularies/http/>
+  PREFIX http: <http://www.w3.org/2011/http#>
   PREFIX prov: <http://www.w3.org/ns/prov#>
   PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
   PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
